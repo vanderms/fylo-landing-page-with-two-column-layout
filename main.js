58be562d7ba00c517566d76dfdececd1e24a5eaa
vanderms <vanderms.84@gmail.com>
2022-02-28 16:02:00 -0300

    implement responsive cta section

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
-const email = document.querySelector('.email-input');
-const form = document.querySelector('form');
-const warning = document.querySelector('.warning');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  warning.classList.add('hidden');
-  email.classList.remove('invalid');
-  form.reset();
-});
 
-console.log(email);
-email.addEventListener('invalid', (e) => {
-  e.preventDefault();
-  warning.classList.remove('hidden');
-  email.classList.add('invalid');
-});
+function initForm(root){
+  const email = root.querySelector('.email-input');
+  const form = root.querySelector('form');
+  const warning = root.querySelector('.warning');
+  
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    warning.classList.add('hidden');
+    email.classList.remove('invalid');
+    form.reset();
+  });  
+ 
+  email.addEventListener('invalid', (e) => {
+    e.preventDefault();
+    warning.classList.remove('hidden');
+    email.classList.add('invalid');
+  });
+}
+
+document.querySelectorAll('.form-container').forEach(initForm);
